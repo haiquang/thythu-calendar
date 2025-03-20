6bb5458a491076eec3242f317bd1fc1c4b70865a
Quang Nguyen <quangnguyen@192.168.1.19>
2025-03-20 11:48:23 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import { AuthProvider } from './context/AuthContext';
 import './App.css';
