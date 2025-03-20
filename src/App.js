e01643c6b9457bf0e444c2d0c2b5f3d70ccf6ad1
Quang Nguyen <quangnguyen@Quangs-MacBook-Pro.local>
2025-03-20 12:50:06 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import { AuthProvider } from './context/AuthContext';
 import './App.css';
@@ -9,7 +9,7 @@ import PrivateRoute from './components/PrivateRoute';
 function App() {
   return (
     <AuthProvider>
-      <Router >
+      <Router basename={process.env.PUBLIC_URL}>
         <div className="App">
           <Routes>
             <Route path="/login" element={<Login />} />
